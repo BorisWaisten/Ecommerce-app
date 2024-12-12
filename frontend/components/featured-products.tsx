09698901314d330ce8cc57dccb0263feccf4f2b5
BorisWaisten <boriswaisten@gmail.com>
2024-12-12 14:20:12 -0300

    .

--- frontend/components/featured-products.tsx
+++ frontend/components/featured-products.tsx
@@ -23,10 +23,11 @@ const FeaturedProducts = () => {
   const router = useRouter();
   const { addItem } = useCart();
   
+  console.log(result +"carrusel");
 
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
-      <h3 className="px-6 text-3xl sm:pb-8">Productos destacados</h3>
+      <h3 className="px-6 text-3xl sm:pb-8 text-center">Productos destacados</h3>
       <Carousel>
         <CarouselContent className="-ml-2 md:-ml-4">
           {loading && <SkeletonSchema grid={3} />}
