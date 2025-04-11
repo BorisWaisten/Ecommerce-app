db3f471d4dff7698630f8e01cce2517851b3efe1
BorisWaisten <boriswaisten@gmail.com>
2025-04-11 11:48:44 -0300

    changes banner

--- frontend/components/banner-product.tsx
+++ frontend/components/banner-product.tsx
@@ -1,15 +1,7 @@
-import Link from "next/link";
-import { buttonVariants } from "./ui/button";
 
 const BannerProduct = () => {
     return (
         <>
-            <div className="mt-4 text-center">
-                <p>Sumérgete en una experiencia única</p>
-                <h4 className="mt-2 text-5xl font-extrabold upperce">Productos</h4>
-                <p className="my-2 text-lg">sloganes de productos</p>
-                <Link href="#" className={buttonVariants()}>Comprar</Link>
-            </div>
             <div className="h-[350px] bg-cover lg:h-[600px] bg-[url('/DEVWAISTEN.png')]  mx-20 bg-center mt-5" />
         </>
     );
